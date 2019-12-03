1a501b1f9eab8f0356d5efd8368704b9e5a1551b
Ronaldo D Rueda Vega <48076023+ronaldorueda@users.noreply.github.com>
2019-12-02 20:26:36 -0600

    Update main.py
    Testing the waters with input and output.

--- lib/main.py
+++ lib/main.py
@@ -1,2 +1,7 @@
 #created and coded by ronaldorueda aka setin07
 #main will get phone number and access json files
+
+nationCode = input("Enter national code number: +")
+phoneNum = input("Enter phone number with following format xxx-xxx-xxxx: ")
+
+print("+" + nationCode + phoneNum)
