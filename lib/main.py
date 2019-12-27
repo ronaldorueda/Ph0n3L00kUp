22ec457dfffd3a0a3eb8e7ed1806ff25fb1d84f2
Ronaldo D Rueda Vega <48076023+ronaldorueda@users.noreply.github.com>
2019-12-27 03:13:29 -0600

    Update main.py
    neatness output

--- lib/main.py
+++ lib/main.py
@@ -6,7 +6,11 @@ nationCode = input("Enter national code number: +")
 phoneNum = input("Enter phone number: ")
 phone = nationCode + phoneNum
 
+print("\n")
+
 print(phone)
+
+print("\n")
 
 #will quit program
 exit = input ("Press enter to exit.")
